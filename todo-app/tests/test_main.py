6f699995ddb3d3d0167a270c6ec1cc306f3c99ec
PabloCacciagioni <152035203+PabloCacciagioni@users.noreply.github.com>
2024-06-05 11:13:47 -0300

    correction of the class name

--- todo-app/tests/test_main.py
+++ todo-app/tests/test_main.py
@@ -4,11 +4,8 @@ This module contains unit tests for the main module.
 import unittest
 from todo_app.main import hello
 
-class TastHello(unittest.TestCase):
+class TestHello(unittest.TestCase):
     def test_hello(self):
-        """
-        Test the hello function.
-        """
         self.assertEqual(hello(), "hello")
         
 if __name__ == '__main__':
