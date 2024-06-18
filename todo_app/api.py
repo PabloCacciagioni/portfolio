1521bd470fc0b719a323e933435a4dd9d2642065
PabloCacciagioni <152035203+PabloCacciagioni@users.noreply.github.com>
2024-06-18 20:28:44 -0300

    Correction7

--- todo_app/api.py
+++ todo_app/api.py
@@ -1,11 +1,6 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from .database import engine, Base, get_db
-from . import models
-
+from fastapi import FastAPI
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
 
 @app.get("/status")
 async def get_status():
--- todo_app/database.py
+++ todo_app/database.py
@@ -1,15 +1,17 @@
 from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 import os
 
 DATABASE_URL = os.getenv("PG_DB_URL", "postgresql://todouser:todopass@localhost:5432/tododb")
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
-
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+    class_=AsyncSession
+)
 async def get_session():
     async with AsyncSession(engine) as session:
         yield session
