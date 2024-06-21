57111ab7f5b0b7d00c596fe3cc3e0752c282eec9
PabloCacciagioni <152035203+PabloCacciagioni@users.noreply.github.com>
2024-06-21 11:27:07 -0300

    sarasa3

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,25 +1,10 @@
-from sqlmodel import SQLModel
+from sqlmodel import Session, create_engine
 from todo_app.models import Todo
 
+engine = create_engine('postgresql://todouser:todopass@localhost:5432/tododb')
 
-def test_todo_model():
-    todo = Todo(title="Test Todo")
-
-    assert isinstance(todo, SQLModel)
-
-    assert todo.title == "Test Todo"
-
-    assert hasattr(todo, 'id')
-
-    expected_attrs = {'id', 'title'}
-    actual_attrs = set(todo.__dict__.keys()) - {'_sa_instance_state'}
-
-    unexpected_attrs = actual_attrs - expected_attrs
-    assert not unexpected_attrs, f"Unexpected attributes found: {unexpected_attrs}"
-
-    todo_dict = todo.dict()
-    expected_dict = {"id": None, "title": "Test Todo"}
-    assert todo_dict == expected_dict, f"Expected {expected_dict}, but got {todo_dict}"
-
-    todo.title = "Updated Todo"
-    assert todo.title == "Updated Todo"
+def test_saving_todo():
+    with Session(engine) as session:
+        todo = Todo(title="Sample Book")
+        session.add(todo)
+        session.commit()
